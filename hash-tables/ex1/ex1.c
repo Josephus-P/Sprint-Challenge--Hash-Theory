e92c5ec82caf0a68330395943edd261bc054b251
Josue Peralta <jperal77@gmail.com>
2018-11-16 13:01:21 -0600

    completed ex1 and ex2

--- hash-tables/ex1/ex1.c
+++ hash-tables/ex1/ex1.c
@@ -6,17 +6,37 @@
 Answer *get_indices_of_item_weights(int *weights, int length, int limit)
 {
   HashTable *ht = create_hash_table(16);
+  Answer *answer = malloc(sizeof(Answer));
 
-  // YOUR CODE HERE
+  for (int i = 0; i < length; i++)
+  {
+    int retrieve = hash_table_retrieve(ht, weights[i]);
+    if (retrieve != -1)
+    {
+      answer->index_1 = i;
+      answer->index_2 = retrieve;
+      destroy_hash_table(ht);
+      return answer;
+    }
+    else
+    {
+      hash_table_insert(ht, limit - weights[i], i);
+    }
+  }
+
+  destroy_hash_table(ht);
 
   return NULL;
 }
 
 void print_answer(Answer *answer)
 {
-  if (answer != NULL) {
+  if (answer != NULL)
+  {
     printf("%d %d\n", answer->index_1, answer->index_2);
-  } else {
+  }
+  else
+  {
     printf("NULL\n");
   }
 }
